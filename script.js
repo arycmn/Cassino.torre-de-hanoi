a9754e86f179c6c4d227f52c1d9c580672ce9593
Juĺia Sperandio <juhsperandio@hotmail.com>
2020-09-14 11:56:57 -0300

    estrutura basica

---
+++ script.js
@@ -0,0 +1,18 @@
+const torre1 = document.getElementById("torre1");
+const torre2 = document.getElementById("torre2");
+const torre3 = document.getElementById("torre3");
+
+const disco1 = document.getElementById("disco1");
+const disco2 = document.getElementById("disco2");
+const disco3 = document.getElementById("disco3");
+const disco4 = document.getElementById("disco4");
+
+//fazer o handler com append no DOM
+
+//verificação de peça de menor / definir peso da peça / condição de movimento de peça
+
+//verificação do número máximo de peças por torre
+
+//contador de cliques
+
+//condição de vitória / mensagem de vitória
